40acdaf86f9919f74a61d262ef327dade6de538a
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-16 22:53:41 -0500

    Started test file

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -1,5 +1,42 @@
 require_relative '../lib/name_generator.rb'
 
 # Define whatever methods you need first
-
+#
 # Now run your actual tests, making sure to do output as necessary
+#
+####################################################################
+
+# does string contain "Xx_"?
+def test_prepend
+  result = myspace_name_generator(name)
+  refute result, "_xXNaMe doesn't include prependage"
+end
+
+
+# does string contain "_xX"?
+def test_append
+  result = myspace_name_generator(name)
+  refute result, "NaMeXx_ doesn't include appendage"
+end
+
+
+# does string contain whitespace?
+def test_whitespace
+  result = myspace_name_generator(name)
+  refute result, "_xXNaMe NaMeXx_ includes whitespace"
+end
+
+
+
+# does string contain punctuation?
+def test_punctuation
+  result = myspace_name_generator(name)
+  refute result, "_xXNaMe?NaMeXx_ doesn't include punctuation"
+end
+
+
+# does string alternate between upper- and lowercase?
+def test_alternate_case
+  result = myspace_name_generator(name)
+  refute result, "_xXNameNameXx_ includes punctuation"
+end
